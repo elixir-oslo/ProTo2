6aa77fe871a426cda59d8ff9788647cb121f81c6
Morten Johansen <morj@ifi.uio.no>
2020-12-15 16:19:02 +0100

    commit

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-HERE=$(dirname $0)
+HERE=$(dirname "$0")
 
-cd $HERE
+cd "$HERE"
 
 export PYTHONPATH=$PYTHONPATH:$HERE/lib
 
 export FLASK_APP=proto2
 #export FLASK_ENV=development
 
-flask run -h 0.0.0.0 -p 5000 2>&1 > flask.log &
+flask run -h 0.0.0.0 -p 5000 > flask.log 2>&1 &
 
 echo $! > flask.pid
 cat flask.pid
