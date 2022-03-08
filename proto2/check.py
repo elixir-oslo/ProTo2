988486f9d96050d42be093058ca114e08001c6c5
Morten Johansen <morj@ifi.uio.no>
2022-03-08 14:31:34 +0100

    the objective is to find the correct entrypoint target via job_id or somehow

--- proto2/check.py
+++ proto2/check.py
@@ -8,6 +8,17 @@ galaxy_history_id = os.getenv('HISTORY_ID')
 
 gi = GalaxyInstance(url=galaxy_url, key=galaxy_api_key)
 
-eps = gi.make_get_request(gi.base_url + '/api/entrypoints', payload={'key': galaxy_api_key})
+jobs = gi.jobs.get_jobs(state='running', tool_id='interactive_tool_proto2', history_id=galaxy_history_id)
+#print(jobs)
 
-print(eps)
+for job in jobs:
+  jid = job['id']
+  jinfo = gi.jobs.show_job(jid, full_details=True)
+  print(jinfo)
+
+eps = gi.make_get_request(gi.base_url + '/api/entry_points?running=true').json()
+
+
+print(eps[0]['target'])
+
+
