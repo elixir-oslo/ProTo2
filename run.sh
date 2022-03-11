1b5065b6a20cdb35bb2b2f6cbc496ff2fff38423
Cloud User <rocky@proto2.norgene.no>
2022-03-11 17:27:08 +0100

    find proxy path and set SCRIPT_NAME for gunicorn/flask

--- run.sh
+++ run.sh
@@ -9,6 +9,9 @@ export PYTHONPATH=$PYTHONPATH:$HERE/lib
 export FLASK_APP=proto2.flask_app
 #export FLASK_ENV=development
 
+export SCRIPT_NAME=$(python /opt/proto/proto2/check.py)
+echo $SCRIPT_NAME
+
 #flask run -h 0.0.0.0 -p 5000 > flask.log 2>&1 &
 gunicorn -b 0.0.0.0:5000 -p flask.pid --log-file flask.log --reload "$FLASK_APP:create_app()"
 
