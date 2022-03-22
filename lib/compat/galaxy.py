4b39fdbe6dcea01c6af6d4630f42c0e6976d3b04
Morten Johansen <morj@ifi.uio.no>
2022-03-22 14:04:14 +0100

    rm relative static hack; rm a whitespace

--- lib/compat/galaxy.py
+++ lib/compat/galaxy.py
@@ -48,11 +48,11 @@ class Transaction:
         self.request.POST = patch_dict(request.form)
 
     def css(self, fname):
-        html = '<link rel="stylesheet" type="text/css" href="./%s/style/%s.css">' % (self.app.static_url_path, fname)
+        html = '<link rel="stylesheet" type="text/css" href="%s/style/%s.css">' % (self.app.static_url_path, fname)
         return html
 
     def js(self, fname):
-        html = '<script type="text/javascript" src="./%s/%s.js"></script>' % (self.app.static_url_path, fname)
+        html = '<script type="text/javascript" src="%s/%s.js"></script>' % (self.app.static_url_path, fname)
         return html
 
     def url_for(self, ref):
