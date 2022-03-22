a370058b3bd3956df24d84b33c834001f276c9b4
Cloud User <rocky@proto2.norgene.no>
2022-03-22 13:35:34 +0100

    fixes

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ export PYTHONPATH=$PYTHONPATH:$HERE/lib
 export FLASK_APP=proto2.flask_app
 #export FLASK_ENV=development
 
-export SCRIPT_NAME=$(python proto2/get_proxy_path.py)
-echo $SCRIPT_NAME
+#export SCRIPT_NAME=$(python proto2/get_proxy_path.py)
+#echo $SCRIPT_NAME
 
 #flask run -h 0.0.0.0 -p 5000 > flask.log 2>&1 &
 gunicorn -b 0.0.0.0:5000 -p flask.pid --log-file flask.log --reload "$FLASK_APP:create_app()"
