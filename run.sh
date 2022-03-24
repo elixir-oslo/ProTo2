c0e81dcbe3c841b8ac5883aa1d633c837e0dd9db
Cloud User <rocky@proto2.norgene.no>
2022-03-24 10:56:03 +0100

    log to job output

--- run.sh
+++ run.sh
@@ -12,13 +12,17 @@ export FLASK_APP=proto2.flask_app
 #export SCRIPT_NAME=$(python proto2/get_proxy_path.py)
 #echo $SCRIPT_NAME
 
+LOG=$GALAXY_OUTPUT
+echo "ProTo2 starting." > $LOG
+#tail -f flask.log >> $GALAXY_OUTPUT &
+
 #flask run -h 0.0.0.0 -p 5000 > flask.log 2>&1 &
-gunicorn -b 0.0.0.0:5000 -p flask.pid --log-file flask.log --reload "$FLASK_APP:create_app()"
+gunicorn -b 0.0.0.0:5000 -p flask.pid --log-file $LOG --reload "$FLASK_APP:create_app()"
 
 #echo $! > flask.pid
 #cat flask.pid
 #trap 'kill $(cat flask.pid)' INT
 #wait
 
-cat flask.log >> $GALAXY_OUTPUT
+#cat flask.log >> $GALAXY_OUTPUT
 
