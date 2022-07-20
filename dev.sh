a07d7330207dbf41c44c70a71850541d4a13a2e6
Sveinung Gundersen <sveinugu@gmail.com>
2022-07-20 10:43:05 -0500

    Updated dev start script

--- dev.sh
+++ dev.sh
@@ -6,7 +6,7 @@ cd $HERE
 
 export PYTHONPATH=$PYTHONPATH:$HERE/lib
 
-export FLASK_APP=proto2.flask_app
+export FLASK_APP=proto2.flask_app:create_app
 export FLASK_ENV=development
 
 flask run -h 0.0.0.0 -p 5000 &
